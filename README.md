0b86e81571bd134e8094ceb5d03cdade65248637
AHPKWEB <152430830+AHPKWEB@users.noreply.github.com>
2024-01-03 00:33:33 +0400

    qp

---
+++ README.md
@@ -0,0 +1,8 @@
+<p align="center">
+    <h1>Welcome to my Quran-player</h1>
+</p>
+
+
+
+Please star a Repository and Share it
+<p>Thanks</p>
---
+++ js/music_list.js
@@ -0,0 +1,37 @@
+let musicList = [
+    {
+        name: "سورة الفاتحة",
+        artist: "آڈیو القرآن",
+        img: "img/sname_1_t.png",
+        src: "audio/Sorat-no-01.mp3",
+        love: false,
+    },
+    {
+        name: "سورة البقرة",
+        artist: "آڈیو القرآن",
+        img: "img/sname_2_t.png",
+        src: "audio/Sorat-no-02.mp3",
+        love: false,
+    },
+    {
+        name: "سورة آل عمران",
+        artist: "آڈیو القرآن",
+        img: "img/sname_3_t.png",
+        src: "audio/Sorat-no-03.mp3",
+        love: false,
+    },
+    {
+        name: "سورة النساء",
+        artist: "آڈیو القرآن",
+        img: "img/sname_4_t.png",
+        src: "audio/Sorat-no-04.mp3",
+        love: false,
+    },
+    {
+        name: "سورة المائدة",
+        artist: "آڈیو القرآن",
+        img: "img/sname_5_t.png",
+        src: "audio/Sorat-no-05.mp3",
+        love: false,
+    },
+];
